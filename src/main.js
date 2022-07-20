05e0f5df27721d48cf84e680aa33f968143de810
Smarandache Stefan <stefan.smarandache96@gmail.com>
2022-07-20 15:30:32 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import "bootstrap/dist/css/bootstrap.min.css"
+import Bootstrap from "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 AOS.init();
-createApp(App).mount('#app')
+createApp(App).use(Bootstrap).mount('#app')
 
 
